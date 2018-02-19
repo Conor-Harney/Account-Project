3029674fe285e6e778babb06d4029b1fd72fc074
Conor-Harney <cw208kccgdbc00155687@gmail.com>
2018-02-19 13:09:20 +0000

    json converter and reader

--- src/test/java/ServiceTester.java
+++ src/test/java/ServiceTester.java
@@ -1,7 +1,11 @@
 import static org.junit.Assert.assertEquals;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
+import com.qa.app.Account;
 import com.qa.app.ServiceCalss;
 
 public class ServiceTester {
@@ -14,5 +18,26 @@ public class ServiceTester {
 		
 		assertEquals("John", ServiceCalss.getAccount(accTrackingNum1).getFirstName());
 	}
+	
+	@Test
+	public void testJsonConversion()
+	{
+		int accTrackingNum1 = ServiceCalss.AddAccount("John", "Doe");;
+		int accTrackingNum2 = ServiceCalss.AddAccount("Jane", "Doe");
+		Account AccAtZero = null;
+		
+		JSONObject jo = ServiceCalss.getJsonMap();
+		try { 
+			
+			AccAtZero = (Account) jo.get("0");
+			 
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		assertEquals("John", AccAtZero.getFirstName());
+		
+		System.out.println("pause");
+	}
+	
 	
 }
